740fd7dce99cf33c44e1e78d1a5bc4b5077b699d
bin xiao <xiaosamaneu@gmail.com>
2024-12-13 13:58:56 -0600

    test v

--- omp/test_v2.cpp
+++ omp/test_v2.cpp
@@ -6,17 +6,21 @@ int reduction(int *array, int num_threads, int array_len)
 {
     omp_set_num_threads(num_threads);
     int group_len = (array_len + num_threads - 1) / num_threads;
+    // First parallel step: local reductions within each group
     #pragma omp parallel for
     for (int i = 0; i < array_len; i += group_len)
     {
-        for (int j = 1; i + j < array_len && j < group_len; j++)
+        int end = std::min(i + group_len, array_len);
+        for (int j = i + 1; j < end; j++)
         {
-            array[i] += array[i + j];
+            array[i] += array[j];
         }
     }
-    #pragma omp parallel for
-    for (int tid = 0; tid < num_threads; tid++)
+
+    // Second parallel step: tree-based reduction across groups
+    #pragma omp parallel
     {
+        int tid = omp_get_thread_num();
         for (int s = num_threads / 2; s > 0; s >>= 1)
         {
             if (tid < s)
@@ -24,14 +28,15 @@ int reduction(int *array, int num_threads, int array_len)
                 array[tid * group_len] += array[(tid + s) * group_len];
             }
             #pragma omp barrier
-        }   
+        }
     }
     return array[0];
 }
 
 using namespace std;
-int main(){
+int main() {
     int array[100];
-    fill(array,array+100,1);
-    cout<<reduction(array,8,100);
+    fill(array, array + 100, 1);
+    cout << reduction(array, 8, 100) << "\n";
+    return 0;
 }
